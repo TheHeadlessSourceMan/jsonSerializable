ddc58f5cadd4c16ea1ad429eddcede8c0a7c2984
keilander <Kurt.Eilander@osii.com>
2026-01-30 08:13:02 -0600

    expose more tools via __init__

--- __init__.py
+++ __init__.py
@@ -4,5 +4,11 @@ a python object serializeable to json.
 
 (Even includes file access!)
 """
+from .types import *
+from .misc import *
+from .jsonTools import *
 from ._jsonSerializeable import *
 from ._JsonMapped import *
+from .jsonFunk import *
+from .infoClass import *
+from .jsonRunner import *
